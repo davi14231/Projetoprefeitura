4e1e0097524fb6d35b5631af447169ad0040291f
Davi Peixoto <davipffalcao@gmail.com>
2025-07-15 13:22:50 -0300

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,8 @@
-import Footer from "@/components/ui/layouts/Footer";
+import BlocoInformativo from "@/components/ui/BlocoInformativo";
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-50">
-      <div className="flex-1 flex items-center justify-center">
-        <h1 className="text-2xl font-bold text-gray-800">Conteúdo de Teste</h1>
-      </div>
-      <Footer />
-    </div>
+      <BlocoInformativo />
   );
 }
 
