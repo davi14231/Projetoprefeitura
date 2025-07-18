577f1894fb95002a10c1ee304ef6f469ef4fc531
DAVI PEIXOTO  FREIRE FALCAO <davipffalcao@gmail.com>
2025-07-17 22:21:26 -0300

    comeco

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import ConfirmacaoEncerrarNecessidade from '@/components/ui/Confirmacaoencerrarnecessidade';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <ConfirmacaoEncerrarNecessidade />
+    </div>
+  );
+}
+
+export default App;
